12e272491eb6cdf9856f0dc2d48488d6c745e1c4
Trimple <andrevdovnar@gmail.com>
2022-04-27 14:31:57 +0300

    Implemented the Joystick visuals

--- source/FlexJoystick.js
+++ source/FlexJoystick.js
@@ -3,20 +3,27 @@ class FlexJoystick {
     outlineObject;
     stickObject;
 
+    #stickSizeFactor = 0.8;
     #internalRadius;
     #externalRadius;
 
     #joystickType;
 
+    #centerX;
+    #centerY;
+
     #width;
     #height;
-
+    #stickWidth;
+    #stickHeight;
 
     constructor(parentId) {
         this.#parentObject = document.getElementById(parentId);
         if (this.#parentObject.tagName != "DIV") {
             return;
         }
+
+        
 
         this.#createOutlineObject();
 
@@ -25,22 +32,48 @@ class FlexJoystick {
 
     #createOutlineObject() {
         this.outlineObject = document.createElement("div");
-        // this.outlineObject.style.opacity = "50%"
+        this.outlineObject.id = this.#parentObject.id + "Outline";
         this.outlineObject.style.position = "relative";
         this.outlineObject.style.margin = "auto";
-        this.outlineObject.style.left = "0px";
 
         this.updateObjectDimensions();
 
         this.outlineObject.style.borderRadius = "100px";
-        // this.outlineObject.style.background = "red";
         this.outlineObject.style.border = "2px solid"
 
         this.#parentObject.appendChild(this.outlineObject);
     }
 
     #createStickObject() {
+        this.stickObject = document.createElement("div");
+        this.stickObject.style.position = "absolute";
+        this.stickObject.style.width = Math.min(this.#width, this.#height) * this.#stickSizeFactor;
+        this.stickObject.style.height = this.stickObject.style.clientWidth;
 
+        this.stickObject.style.background = "red";
+
+        this.#updateStickDimensions();
+
+        this.outlineObject.appendChild(this.stickObject);
+    }
+    
+    #updateStickDimensions()
+    {
+        this.#stickWidth = this.stickObject.clientWidth;
+        this.#stickHeight = this.stickObject.clientHeight;
+    }
+
+
+
+    updateObjectDimensions() {
+        this.#evaluateJoystickType();
+        // center round border in case of rectangular Parent
+        this.outlineObject.style.top = (this.#parentObject.clientHeight - this.#height) / 2 + 'px';
+
+        this.outlineObject.style.width = this.#width + 'px';
+        this.outlineObject.style.height = this.#height + 'px';
+        this.#centerX = this.#width/2;
+        this.#centerY = this.#height/2;
     }
 
     #evaluateJoystickType() {
@@ -60,12 +93,5 @@ class FlexJoystick {
         }
     }
 
-    updateObjectDimensions() {
-        this.#evaluateJoystickType();
-        this.outlineObject.style.top = (this.#parentObject.clientHeight - this.#height) / 2 + 'px';
-
-        this.outlineObject.style.width = this.#width + 'px';
-        this.outlineObject.style.height = this.#height + 'px';
-    }
 
 }
