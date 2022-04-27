18f5e9cda45d3108eb8f64d1c221c375b83666d8
Trimple <andrevdovnar@gmail.com>
2022-04-27 17:17:36 +0300

    fixed round joystick movement

--- source/FlexJoystick.js
+++ source/FlexJoystick.js
@@ -12,10 +12,14 @@ class FlexJoystick {
     #currentX;
     #currentY;
 
-    #width;
-    #height;
+    #parentWidth;
+    #parentHeight;
+    #outlineWidth;
+    #outlineHeight;
     #stickWidth;
     #stickHeight;
+    #heightOffset;
+    #widthOffset;
 
     #isPressed = false;
     #touchId = -1;
@@ -26,12 +30,11 @@ class FlexJoystick {
             return;
         }
 
-        
-
         this.#createOutlineObject();
-
+        
         this.#createStickObject();
     }
+
 
     #createOutlineObject() {
         this.#outlineObject = document.createElement("div");
@@ -39,13 +42,48 @@ class FlexJoystick {
         this.#parentObject.appendChild(this.#outlineObject);
 
         this.#outlineObject.style.position = "absolute";
-        this.#outlineObject.style.margin = "auto";
 
         this.updateObjectDimensions();
 
         this.#outlineObject.style.borderRadius = "100px";
         this.#outlineObject.style.border = "2px solid"
+    }
 
+    updateObjectDimensions() {
+        this.#evaluateJoystickType();
+        // center round border in case of rectangular Parent
+        this.#outlineObject.style.top = this.#heightOffset + 'px';
+        this.#outlineObject.style.left = this.#widthOffset + 'px';
+
+        this.#centerX = this.#parentWidth / 2;
+        this.#centerY = this.#parentHeight / 2;
+
+        this.#outlineObject.style.width = this.#outlineWidth  + 'px';
+        this.#outlineObject.style.height = this.#outlineHeight + 'px';
+    }
+
+    #evaluateJoystickType() {
+        this.#parentWidth = this.#parentObject.clientWidth;
+        this.#parentHeight = this.#parentObject.clientHeight;
+
+        this.#outlineWidth = this.#parentWidth;
+        this.#outlineHeight = this.#parentHeight;
+
+        
+        if (this.#parentWidth / this.#parentHeight > 1.4) {
+            this.#joystickType = "wide";
+        }
+        else if (this.#parentHeight / this.#parentWidth > 1.4) {
+            this.#joystickType = "long";
+        }
+        else {
+            this.#joystickType = "round";
+            this.#outlineWidth = Math.min(this.#parentWidth, this.#parentHeight);
+            this.#outlineHeight = this.#outlineWidth;
+        }
+
+        this.#widthOffset = (this.#parentWidth - this.#outlineWidth) / 2;
+        this.#heightOffset = (this.#parentHeight - this.#outlineHeight) / 2;
     }
 
     #createStickObject() {
@@ -54,12 +92,11 @@ class FlexJoystick {
         this.#parentObject.appendChild(this.#stickObject);
 
         this.#stickObject.style.position = "absolute";
-        if(this.#joystickType === "round")
-        {
-            this.#stickObject.style.width = Math.min(this.#width, this.#height) * this.#stickSizeFactor + "px";
+        if (this.#joystickType === "round") {
+            this.#stickObject.style.width = Math.min(this.#outlineWidth, this.#outlineHeight) * this.#stickSizeFactor + "px";
             this.#stickObject.style.height = this.#stickObject.clientWidth + "px";
-        }else{
-            this.#stickObject.style.width = Math.min(this.#width, this.#height) * this.#stickSizeFactor*2 + "px";
+        } else {
+            this.#stickObject.style.width = Math.min(this.#outlineWidth, this.#outlineHeight) * this.#stickSizeFactor * 2 + "px";
             this.#stickObject.style.height = this.#stickObject.clientWidth + "px";
         }
 
@@ -67,99 +104,103 @@ class FlexJoystick {
 
         this.#stickObject.style.borderRadius = "100px";
         this.#stickObject.style.background = "red";
-        
-        this.#currentX = this.#centerX - this.#stickHeight/2;
-        this.#currentY = this.#centerY - this.#stickWidth/2;
 
-        this.#stickObject.style.left = this.#currentX + "px";
-        this.#stickObject.style.top = this.#currentY + "px";
+        this.#moveStick(this.#centerX, this.#centerY);
     }
-    
-    #updateStickDimensions()
-    {
+
+    #updateStickDimensions() {
         this.#stickWidth = this.#stickObject.clientWidth;
         this.#stickHeight = this.#stickObject.clientHeight;
     }
 
+    #moveStick(targetX, targetY) {
+        this.#currentX = targetX - this.#stickHeight / 2;
+        this.#currentY = targetY - this.#stickWidth / 2;
 
-
-    updateObjectDimensions() {
-        this.#evaluateJoystickType();
-        // center round border in case of rectangular Parent
-        this.#outlineObject.style.top = (this.#parentObject.clientHeight - this.#height) / 2 + 'px';
-
-        this.#outlineObject.style.width = this.#width + 'px';
-        this.#outlineObject.style.height = this.#height + 'px';
-        this.#centerX = this.#width/2;
-        this.#centerY = this.#height/2;
+        this.#stickObject.style.left = this.#currentX + "px";
+        this.#stickObject.style.top = this.#currentY + "px";
     }
 
-    #evaluateJoystickType() {
-        this.#width = this.#parentObject.clientWidth;
-        this.#height = this.#parentObject.clientHeight;
+    #moveWide(targetX, targetY) {
+        // if (targetX > this.#parentObject.clientWidth - this.#stickWidth / 2) {
+        //     targetX = this.#parentObject.clientWidth - this.#stickWidth / 2;
+        // }
+        // else if (targetX < this.#stickWidth / 2) {
+        //     targetX = this.#stickWidth / 2;
+        // }
+        // targetY = this.#height / 2;
 
-        if (this.#parentObject.clientWidth / this.#parentObject.clientHeight > 1.4) {
-            this.#joystickType = "wide";
+        // this.#moveStick(targetX, targetY);
+
+        // // this.#stickObject.style.left = targetX - this.#stickWidth/2 + "px";
+        // // this.#stickObject.style.top = targetY - this.#stickHeight/2 + "px";
+    }
+
+    #moveLong(targetX, targetY) {
+
+    }
+
+    #moveRound(targetX, targetY) {
+        if (targetX < this.#stickWidth / 2 + this.#widthOffset) {
+            targetX = this.#stickWidth / 2 + this.#widthOffset;
         }
-        else if (this.#parentObject.clientHeight / this.#parentObject.clientWidth > 1.4) {
-            this.#joystickType = "long";
+        if (targetX > this.#parentWidth - this.#stickWidth / 2 - this.#widthOffset) {
+            targetX = this.#parentWidth - this.#stickWidth / 2 - this.#widthOffset;
         }
-        else {
-            this.#width = Math.min(this.#parentObject.clientWidth, this.#parentObject.clientHeight);
-            this.#height = this.#width;
-            this.#joystickType = "round";
+
+        if (targetY < this.#stickHeight / 2 + this.#heightOffset) {
+            targetY = this.#stickHeight / 2 + this.#heightOffset;
         }
+        if (targetY > this.#parentHeight - this.#stickHeight / 2 - this.#heightOffset) {
+            targetY = this.#parentHeight - this.#stickHeight / 2 - this.#heightOffset;
+        }
+
+        this.#moveStick(targetX, targetY);
     }
 
     handleMouseDown(event) {
         this.#isPressed = true;
+
         let clickX = event.pageX - this.#parentObject.offsetLeft;
         let clickY = event.pageY - this.#parentObject.offsetTop;
 
-        this.#stickObject.style.top = clickY - this.#stickHeight/2 + "px";
-        this.#stickObject.style.left = clickX - this.#stickWidth/2 + "px";
-
+        if (this.#joystickType === "long") {
+            this.#moveLong(clickX, clickY);
+        }
+        else if (this.#joystickType === "wide") {
+            this.#moveWide(clickX, clickY);
+        }
+        else {
+            this.#moveRound(clickX, clickY);
+        }
     }
 
     handleMouseMove(event) {
-        if(this.#isPressed === false)
-        {
+        if (this.#isPressed === false) {
             return;
         }
+
         let clickX = event.pageX - this.#parentObject.offsetLeft;
         let clickY = event.pageY - this.#parentObject.offsetTop;
 
-        if(clickX < this.#stickWidth/2)
-        {
-            clickX = this.#stickWidth/2;
+        if (this.#joystickType === "long") {
+            this.#moveLong(clickX, clickY);
         }
-        if(clickX > this.#width - this.#stickWidth/2)
-        {
-            clickX = this.#width - this.#stickWidth/2;
+        else if (this.#joystickType === "wide") {
+            this.#moveWide(clickX, clickY);
         }
-
-        if(clickY < this.#stickWidth/2)
-        {
-            clickY = this.#stickWidth/2;
+        else {
+            this.#moveRound(clickX, clickY)
         }
-        if(clickY > this.#height - this.#stickHeight/2)
-        {
-            clickY = this.#height - this.#stickHeight/2;
-        }
-
-        this.#stickObject.style.top = clickY - this.#stickHeight/2 + "px";
-        this.#stickObject.style.left = clickX - this.#stickWidth/2 + "px";
     }
 
     handleMouseUp(event) {
-        if(this.#isPressed === false)
-        {
+        if (this.#isPressed === false) {
             return;
         }
         this.#isPressed = false;
 
-        this.#stickObject.style.left = this.#centerX - this.#stickWidth/2 + "px";
-        this.#stickObject.style.top = this.#centerY - this.#stickHeight/2 + "px";
+        this.#moveStick(this.#centerX, this.#centerY);
     }
 
     handleTouchStart(event) {
@@ -175,10 +216,10 @@ class FlexJoystick {
     }
 
     getStickX() {
-        return this.#centerX;
+        return this.#currentX + this.#stickWidth / 2 - this.#parentWidth / 2;
     }
 
-    getStickY(){
-        return this.#currentY;
+    getStickY() {
+        return this.#currentY + this.#stickHeight / 2 - this.#parentHeight / 2;
     }
 }
